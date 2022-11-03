33156444928864968c7ec816746912f3452bba78
Z-mh666 <78141281+Z-mh666@users.noreply.github.com>
2022-11-03 22:58:07 +0800

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -25,10 +25,11 @@ void jacobi(SpMatrix& A, Vector& b, Vector& u, Mesh& mesh, double tol, int maxit
     }
   }
   exchangeAddInterfMPI(Mdiag, mesh);
-  
+
   // Jacobi solver
   double residuNorm = 1e2;
   int it = 0;
+  Vector w(size);
   while (residuNorm > tol && it < maxit){
     
     // Compute N*u
@@ -39,11 +40,22 @@ void jacobi(SpMatrix& A, Vector& b, Vector& u, Mesh& mesh, double tol, int maxit
     for(int i=0; i<size; i++){
       u(i) = 1/Mdiag(i) * (Nu(i) + b(i));
     }
-    
+
     // Update residual and iterator
+    
     if((it % 10) == 0){
-      if(myRank == 0)
-        printf("\r   %i %e", it, residuNorm);
+      double residu = 0;
+      Vector Au = A*u;
+      exchangeAddInterfMPI(Au, mesh);
+      
+      for (int i=0;i<size;i++){
+        w(i) = b(i)-Au(i);
+        residu += w(i)*w(i);
+      }
+  
+      if(myRank == 0){
+        printf("\r   %i %e", it, residu);
+      }
     }
     it++;
   }
@@ -51,5 +63,21 @@ void jacobi(SpMatrix& A, Vector& b, Vector& u, Mesh& mesh, double tol, int maxit
   if(myRank == 0){
     printf("\r   -> final iteration: %i (prescribed max: %i)\n", it, maxit);
     printf("   -> final residual: %e (prescribed tol: %e)\n", residuNorm, tol);
+  }
+}
+
+void normL2(SpMatrix& M,Vector& v,Mesh& mesh)
+{
+  double  r = 0;
+  Vector Mv = M*v;
+  exchangeAddInterfMPI(Mv,mesh);
+  for (int i=0;i<nbTasks;i++){
+    double rr = v.dot(Mv);
+    if (myRank == i){
+      MPI_Reduce(&rr, &r, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    }
+  }
+  if (myRank == 0){
+    printf("   -> final L2 error: %e\n",sqrt(r));
   }
 }
